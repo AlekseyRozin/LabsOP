6c0c11eeaa69337cfe202dce992b77e2aed29dd7
Aleksey Rozin <nen.tim23@gmail.com>
2021-09-24 15:51:17 +0300

    Вторая лабораторная работа ОП

---
+++ lab2/lab2.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    cout << "Введите количсетво минут: ";
+    int time; //количество минут, которые прошли с начала часа
+    cin >> time; 
+    if (time % 5 <= 2) {
+        cout << "Сейчас горит зелёный";
+    }
+    else {
+        cout << "Сейчас горит красный";
+    }
+}
