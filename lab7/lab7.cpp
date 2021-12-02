6f62970e7bfd767aa42652377f5bd3d8ae5f3f28
Aleksey Rozin <nen.tim23@gmail.com>
2021-12-02 11:32:22 +0200

    !

--- lab7/lab7.cpp
+++ lab7/lab7.cpp
@@ -2,7 +2,9 @@
 using namespace std;
 void generateArr(int* p, int n); // функция для заполнения массива случайными числами
 void outputArr(int array[], int n); // функция для вывода элементов массива в консоль
-int maxMinDiff(int array[], int n); // функция для поиска разницы максимального и минимального значения массива
+int findMin(int array[], int n); // функция для поиска минимального элемента массива
+int findMax(int array[], int n); // функция для поиска максимального элемента массива
+int maxMinDiff(int min, int max); // функция для поиска разницы максимального и минимального значения массива
 void changeArr(int* p, int n, int diff); // функция для присовения каждому второму элемента массива значение разности
 
 int main()
@@ -10,16 +12,18 @@ int main()
     int n;
     cout << "Enter array length: "; cin >> n;
     int* arr = new int[n];
+
     generateArr(arr, n);
     cout << "\nGenerated array: "; outputArr(arr, n);
-    int diff = maxMinDiff(arr, n);
+
+    int diff = maxMinDiff(findMin(arr,n), findMax(arr,n));
     cout << "Difference of max and min element: " << diff << "\n\n";
+
     changeArr(arr, n, diff);
     cout << "Changed array: "; outputArr(arr, n);
 
     // освобождаем выделенную массиву память
     delete[] arr;
-    arr = 0;
 }
 
 void generateArr(int* p, int n) {
@@ -36,18 +40,25 @@ void outputArr(int array[], int n) {
     }
     cout << "\n\n";
 }
-
-int maxMinDiff(int array[], int n) {
-    int max = array[0];
+int findMin(int array[], int n) {
     int min = array[0];
+    for (int i = 1; i < n; i++) {
+        if (array[i] < min) {
+            min = array[i];
+        }
+    }
+    return min;
+}
+int findMax(int array[], int n) {
+    int max = array[0];
     for (int i = 1; i < n; i++) {
         if (array[i] > max) {
             max = array[i];
         }
-        else if (array[i] < min) {
-            min = array[i];
-        }
     }
+    return max;
+}
+int maxMinDiff(int min, int max) {
     return max - min;
 }
 
